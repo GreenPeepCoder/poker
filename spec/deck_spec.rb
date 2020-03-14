1c62a66fce673635e0d1c33b9915aea9b2e153e5
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-14 11:42:16 -0400

    Implement specs for Deck::all_cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,6 +3,17 @@ require 'deck'
 
 describe Deck do
     describe "::all_cards" do
+        subject(:all_cards) {Deck.all_cards}
+
+        it 'should generate 52 cards' do
+            expect(all_cards.count).to eq(52)
+        end
+
+        it 'returns all cards without duplicates' do
+            expect(
+                all_cards.map { |card| [card.value, card.suit]}.uniq.count
+            ).to eq(all_cards.count)
+        end
     end
 
     describe "#initialize" do
