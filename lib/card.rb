cd0b39b097de624e0cfb5f3392b8f832a91a9fcb
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-14 10:54:23 -0400

    Finish specs and code for Card#initialize(value, suit)

--- lib/card.rb
+++ lib/card.rb
@@ -4,18 +4,18 @@ class Card
     SUITS = %i(spades hearts diamonds clubs)
 
     VALUE_STRINGS = {
-        :two => "2"
-        :three => "3"
-        :four => "4"
-        :five => "5"
-        :six => "6"
-        :seven => "7"
-        :eight => "8"
-        :nine => "9"
-        :ten => "10"
-        :jack => "J"
-        :queen => "Q"
-        :king => "K"
+        :two => "2",
+        :three => "3",
+        :four => "4",
+        :five => "5",
+        :six => "6",
+        :seven => "7",
+        :eight => "8",
+        :nine => "9",
+        :ten => "10",
+        :jack => "J",
+        :queen => "Q",
+        :king => "K",
         :ace => "A"
     }
 
@@ -25,5 +25,16 @@ class Card
 
     def self.values
         VALUE_STRINGS.values
+    end
+    
+    attr_reader :value, :suit
+
+    def initialize(value, suit)
+        if VALUE_STRINGS.values.include?(value) == false
+            raise "Invalid card value"
+        elsif SUITS.include?(suit) == false
+            raise "Invalid card suit"
+        end
+        @value, @suit = value, suit
     end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,6 +3,24 @@ require 'card'
 
 describe Card do
     describe "#initialize" do
+        subject(:card) {Card.new("A", :spades)}
+
+        it 'sets up a card correctly' do
+            expect(card.suit).to eq(:spades)
+            expect(card.value).to eq("A")
+        end
+
+        it 'raises an error when given invalid suit arg' do
+            expect do
+                Card.new("J", "hearts")
+            end.to raise_error("Invalid card suit")
+        end
+
+        it 'raises an error when given invalid value arg' do
+            expect do
+                Card.new(9, :hearts)
+            end.to raise_error("Invalid card value")
+        end
     end
 
     describe "#<=>" do
