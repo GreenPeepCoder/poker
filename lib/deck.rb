e440473355268f25e7c7b064a669e0838dbf8c22
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-14 11:51:39 -0400

    Implement Deck::all_cards - generates a 52 card deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,5 +2,15 @@ require_relative './card'
 require_relative './hand'
 
 class Deck
+    
+    def self.all_cards
+        deck = []
+        Card::SUITS.each do |suit|
+            Card::VALUE_STRINGS.values.each do |value|
+                deck << Card.new(value, suit)
+            end
+        end
+        deck
+    end
 
 end
