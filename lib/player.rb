d7edbd3e7adda5aa7b4a869a4eb15080b006ef08
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-19 19:18:39 -0400

    Outline main specs and methods for the Player class

---
+++ lib/player.rb
@@ -0,0 +1,28 @@
+class Player
+
+    include Comparable
+
+    def self.buy_in(bankroll)
+    end
+
+    def initialize(bankroll)
+    end
+
+    def deal_in(hand)
+    end
+
+    def take_bet(total_bet)
+    end
+
+    def receive_winnings(amount)
+    end
+
+    def return_cards
+    end
+
+    def fold
+    end
+
+    def folded?
+    end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require 'player'
+
+describe Player do
+
+    describe '::buy_in' do
+    end
+
+    describe '::deal_in' do
+    end
+
+    describe '#take_bet' do
+    end
+
+    describe '#receive winnings' do
+    end
+
+    describe '#return_cards' do
+    end
+
+    describe '#fold' do
+    end
+
+    describe '#folded?' do
+    end
+end
