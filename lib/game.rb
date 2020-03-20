56ea038df889cf859b698f63eda004c510fda947
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-20 17:25:11 -0400

    Add and outline files for Game and Game_spec

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+require_relative "player"
+require_relative "deck"
+
+class Game
+    attr_reader :players, :pot, :deck
+
+    def initialize
+    end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'game'
+
+describe Game do
+    subject(:game) {Game.new}
+
+    describe '#initialize' do
+    end
+
+    describe '#deck' do
+    end
+
+    describe '#add_players' do
+    end
+
+    describe '#game_over?' do
+    end
+
+    describe '#deal_cards' do
+    end
+
+    describe '#add_to_pot' do
+    end
+end
